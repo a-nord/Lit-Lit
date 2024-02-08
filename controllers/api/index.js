c578609907dfe8578372585d6cf87616f5fee593
Ashley Nord <anord@hntb.com>
2024-02-08 10:35:28 -0500

    imported all of the routes and connected the routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 // Import all of the routes from /api/ here
+const userRoutes = require('./userRoutes');
+const dataOneRoute = require('./dataOneRoute');
 
 // Connect the routes to the router here
+router.use('/users', userRoutes);
+router.use('/dataOneRoute', dataOneRoute);
 
 module.exports = router;
