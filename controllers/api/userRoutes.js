02904e8bce6e45d62b1de11aa94b252a8f8546d1
Ashley Nord <anord@hntb.com>
2024-02-08 10:24:24 -0500

    added a post logout API route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,11 +25,33 @@ router.post('/', async (req, res) => {
 // add a post login API route here
 router.post('/login', async (req, res) => {
   try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
     console.log('User attempting to log in');
   } catch (err) {
-    console.log('There was an error logging in');
-    console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
