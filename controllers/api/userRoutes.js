6677e522aadcfe4091b19d24118b8bf929f7dabd
Ashley Nord <anord@hntb.com>
2024-02-08 10:19:16 -0500

    added a post user API route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
 
 // import any models you plan to use for this data's routes here
+const { User } = require('../../models');
 
 // /api/
 // add a post user API route here
 router.post('/', async (req, res) => {
   try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
     console.log('User attempting to be added');
-  } catch (err) {
-    console.log('There was an error creating user');
-    console.log(err);
-    res.status(500).json(err);
+  } catch (err) {    
+    res.status(400).json(err);
   }
 });
 
