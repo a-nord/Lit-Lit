882fa6b7a9cbae596a2157c4e43bdd80020b3e68
Mike <nguyen.mike1092@gmail.com>
2024-02-12 12:34:45 -0500

    got axios to grab data

--- controllers/api/results.js
+++ controllers/api/results.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const { Books } = require('../../models');
 
-router.post('/', (req, res) => {
-    const { title, author, cover } = req.body;
-  
-    res.render('results', { title, author, cover });
-  });
-  
-  module.exports = router;
+router.post('/', async (req, res) => {
+  //console.log(req.body);
+  const response = await axios.get(
+    `https://openlibrary.org/search.json?title=${req.body}`
+  );
+  // const results = response;
+  console.log(response.data.docs[0].key);
+});
+
+// router.post('/', (req, res) => {
+//   const { title, author, cover } = req.body;
+
+//   res.render('results', { title, author, cover });
+// });
+
+module.exports = router;
